1fab4e8d6063faf136a64c2c3a498dcb73fd2571
Whittle <matt.whittle10@gmail.com>
2019-05-19 18:09:43 -0600

    basic layout of page created

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+// Arrays to store the different word that will be guessed. 
+
+var songs90s = [];
+var songs2000s = [];
+var artists90s = [];
+var artists2000s = [];
+
+//function that will select which array the user wants
+
+
+
+//function that will be used to randomly select and pull an object from the array.
+
+var randSongs90s = songs90s[Math.floor(Math.random() * songs90.length)];
+var randSongs2000s = songs2000s[Math.floor(Math.random() * songs2000s.length)];
+var randArtists90s = artists90s[Math.floor(Math.random() * artists90s.length)];
+var randArtists2000s = artists2000s[Math.floor(Math.random() * artists2000s.length)];
+
+//variable that will be used to store the object that was pulled from the array.
+
+//blank array that the user will be prompted to enter a letter into each time limiting them to 7 attempts before they lose.
+
+//function that will count the number of attempts.
+
+//function that will input the correct letter if it's guessed by the user.
+
+//loops that will be used when entering in letters to check for any matching letters on the object that was pulled from the array.
+
+//alert that will tell the user if they have won or lost.
